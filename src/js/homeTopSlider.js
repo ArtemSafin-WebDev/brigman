dcc3ee47280f36270cc8d856c1b5ed632e68b219
Artem Safin <artemsafin.webdev@gmail.com>
2021-05-19 19:09:06 +0300

    Фикс слайдера

--- src/js/homeTopSlider.js
+++ src/js/homeTopSlider.js
@@ -7,6 +7,29 @@ export default function homeTopSlider() {
 
     elements.forEach(element => {
         const container = element.querySelector('.swiper-container');
+        const swiperSlides = Array.from(element.querySelectorAll('.swiper-slide'));
+        const containerWidth = document.querySelector('.container').offsetWidth;
+
+        if (!window.matchMedia('(max-width: 640px)').matches && swiperSlides.length) {
+            const slidesTotalLength = swiperSlides.reduce((accumulator, currentItem) => {
+                return accumulator + currentItem.offsetWidth;
+            }, 0);
+
+            const timesToDuplicate = Math.ceil(containerWidth / slidesTotalLength);
+
+            if (timesToDuplicate > 1) {
+                // console.log('Slides total length', slidesTotalLength);
+                // console.log('Container width', containerWidth);
+                // console.log('Times to duplicate', timesToDuplicate);
+    
+                for (let i = 0; i < timesToDuplicate; i++) {
+                    // console.log('Running loop');
+                    swiperSlides.forEach(slide => {
+                        slide.parentElement.appendChild(slide.cloneNode(true));
+                    });
+                }
+            } 
+        }
 
         const options = {
             slidesPerView: 'auto',
@@ -15,15 +38,16 @@ export default function homeTopSlider() {
             speed: 500,
             loop: window.matchMedia('(max-width: 640px)').matches ? false : true,
             loopedSlides: 7,
-            centeredSlides: window.matchMedia('(max-width: 640px)').matches ? false : true,
-        }
+            loopAdditionalSlides: 7,
+            centeredSlides: window.matchMedia('(max-width: 640px)').matches ? false : true
+        };
 
         if (!window.matchMedia('(max-width: 768px)').matches) {
             options.speed = 3500;
             options.autoplay = {
                 delay: 0,
                 disableOnInteraction: true
-            }
+            };
 
             element.classList.add('slider-with-autoplay');
         }
@@ -31,18 +55,13 @@ export default function homeTopSlider() {
         const slider = new Swiper(container, options);
 
         if (!window.matchMedia('(max-width: 768px)').matches) {
-            
             element.addEventListener('mouseenter', () => {
                 slider.autoplay.stop();
                 console.log('Autoplay stopped');
-
-                
             });
             element.addEventListener('mouseleave', () => {
                 slider.autoplay.start();
                 console.log('Autoplay started');
-
-                
             });
         }
     });
